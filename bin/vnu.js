eb88382f3ffbe0d415624c9e80d74b06b27c68d1
Robin Boers <robindev2019@outlook.com>
2024-04-07 21:03:40 +0200

    Small fixes

--- bin/vnu.js
+++ bin/vnu.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-const { spawn } = require('child_process');
-const vnu = require('vnu-jar');
+const { spawn } = require("child_process");
+const jar = require("vnu-jar");
 
 // The first two arguments will be ['node', 'thisscript']
 const args = process.argv.slice(2);
 
-const process = spawn('java', ['-jar', vnu, ...args], { stdio: 'inherit' });
+const vnu = spawn("java", ["-jar", jar, ...args], { stdio: "inherit" });
 
-process.on('exit', (code, signal) => {
+vnu.on("exit", (code, signal) => {
   if (code !== null) process.exit(code);
 
   console.error(`Child process terminated with signal ${signal}`);
